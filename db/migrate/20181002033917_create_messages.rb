f0ed6df5b17fae3681fe143b01a2896f19bdfb72
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-04 20:49:56 +0900

    Add  HTML CSS

--- db/migrate/20181002033917_create_messages.rb
+++ db/migrate/20181002033917_create_messages.rb
@@ -1,11 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-    	t.text			:body
-    	t.text			:image
-    	t.integer		:user_id, null: false, foreign_key: true
-    	t.integer		:group_id, null: false, foreign_key: true
-        t.timestamps
+      t.text			:body
+      t.text			:image
+      t.integer		:user_id, null: false, foreign_key: true
+      t.integer		:group_id, null: false, foreign_key: true
+      t.timestamps
     end
   end
 end
