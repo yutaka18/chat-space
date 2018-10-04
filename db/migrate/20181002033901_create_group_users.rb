9458896b6c44d390d55a93ea5c358a72381ceff6
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-04 15:24:06 +0900

    Add  haml sass

---
+++ db/migrate/20181002033901_create_group_users.rb
@@ -0,0 +1,9 @@
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_users do |t|
+     	t.integer		:user_id, null: false, foreign_key: true
+    	t.integer		:group_id, null: false, foreign_key: true
+    	t.timestamps
+    end
+  end
+end
