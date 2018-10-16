a8f72579e7e151a9c3a4a4a994e873fdf273b69d
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-16 12:53:29 +0900

    Add(インクリメンタルサーチの実装)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?) and name !=?',"%#{params[:keyword]}%" , current_user.name)
+    @users = User.where('name LIKE(?) and id !=?',"%#{params[:keyword]}%" , current_user.id)
     respond_to do |format|
       format.html
       format.json
