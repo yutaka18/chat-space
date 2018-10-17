a35f266be65cb55c46d4cc5d11f62600f4c1a14d
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-17 12:41:23 +0900

    Add(自動更新機能の実装)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,7 +27,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:id, :body, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 
   def set_group
